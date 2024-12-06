531ab16286836beee7853222d8fc74736ffd48c1
user638726 <lienjackson35@gmail.com>
2024-12-06 15:48:15 +0800

    改功能

--- backend/title.php
+++ backend/title.php
@@ -13,7 +13,7 @@
     </table>
     <div style="width:99%; height:87%; margin:auto; overflow:auto; border:#666 1px solid;">
         <p class="t cent botli">網站標題管理</p>
-        <form method="post" action="./api/edit_<?=$do;?>.php">
+        <form method="post" action="./api/edit.php">
             <table width="100%">
                 <tbody>
                     <tr class="yel">
@@ -60,6 +60,7 @@
                                 onclick="op(&#39;#cover&#39;,&#39;#cvr&#39;,&#39;./modal/<?=$do;?>.php&#39;)"
                                 value="新增網站標題圖片"></td>
                         <td class="cent">
+                        <input type="hidden" name="table" value="<?=$do;?>">
                             <input type="submit" value="修改確定">
                             <input type="reset" value="重置">
                         </td>
