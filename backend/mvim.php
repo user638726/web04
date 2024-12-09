1393f5850451187a305369300854349f218e00c6
user638726 <lienjackson35@gmail.com>
2024-12-09 14:54:03 +0800

    修改一下

--- backend/mvim.php
+++ backend/mvim.php
@@ -48,6 +48,7 @@
                     ?>
                 </tbody>
             </table>
+            
             <table style="margin-top:40px; width:70%;">
                 <tbody>
                     <tr>
