b089f5c130d87c9ebdcfac4571f5b9a64ca9dabc
user638726 <lienjackson35@gmail.com>
2024-12-13 14:24:50 +0800

    增加跑馬燈的功能

--- admin.php
+++ admin.php
@@ -1,4 +1,4 @@
-﻿﻿﻿<?php include_once "api/db.php";?>
+﻿﻿<?php include_once "api/db.php";?>
 <!DOCTYPE html
     PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <!-- saved from url=(0068)?do=admin&redo=title -->
@@ -75,7 +75,7 @@
                 </div>
                 <div class="dbor" style="margin:3px; width:95%; height:20%; line-height:100px;">
                     <span class="t">進站總人數 :
-                        1 </span>
+                        <?=$Total->find(1)['total'];?></span>
                 </div>
             </div>
             <?php
@@ -93,7 +93,7 @@
         <div style="clear:both;"></div>
         <div
             style="width:1024px; left:0px; position:relative; background:#FC3; margin-top:4px; height:123px; display:block;">
-            <span class="t" style="line-height:123px;"></span>
+            <span class="t" style="line-height:123px;"><?=$Bottom->find(1)['bottom'];?></span>
         </div>
     </div>
 
