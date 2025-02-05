2caefe01dea7c8a6bc9f361668d55c99692bcb50
user638726 <lienjackson35@gmail.com>
2025-02-05 08:07:56 +0800

    改

--- admin.php
+++ admin.php
@@ -28,13 +28,13 @@ if(!isset($_SESSION['login'])){
                 onclick="cl(&#39;#cover&#39;)">X</a>
             <div id="cvr" style="position:absolute; width:99%; height:100%; margin:auto; z-index:9898;">
 
-			</div>
+            </div>
         </div>
     </div>
 
     <div id="main">
         <a title="<?=$Title->find(['sh'=>1])['text'];?>" href="index.php">
-            <div class="ti" style="background:url('./upload/<?=$Title->find(['sh'=>1])['img'];?>'); background-size:cover;"></div>
+
             <!--標題-->
         </a>
         <div id="ms">
@@ -42,19 +42,7 @@ if(!isset($_SESSION['login'])){
                 <div id="menuput" class="dbor">
                     <!--主選單放此-->
                     <span class="t botli">後台管理選單</span>
-                    <a style="color:#000; font-size:13px; text-decoration:none;" href="?do=title">
-                        <div class="mainmu">
-                            網站標題管理 </div>
-                            
-                    </a>
-                    <a style="color:#000; font-size:13px; text-decoration:none;" href="?do=ad">
-                        <div class="mainmu">
-                            動態文字廣告管理 </div>
-                    </a>
-                    <a style="color:#000; font-size:13px; text-decoration:none;" href="?do=mvim">
-                        <div class="mainmu">
-                            動畫圖片管理 </div>
-                    </a>
+
                     <a style="color:#000; font-size:13px; text-decoration:none;" href="?do=image">
                         <div class="mainmu">
                             校園映象資料管理 </div>
@@ -67,34 +55,23 @@ if(!isset($_SESSION['login'])){
                         <div class="mainmu">
                             頁尾版權資料管理 </div>
                     </a>
-                    <a style="color:#000; font-size:13px; text-decoration:none;" href="?do=news">
-                        <div class="mainmu">
-                            最新消息資料管理 </div>
-                    </a>
-                    <a style="color:#000; font-size:13px; text-decoration:none;" href="?do=admin">
-                        <div class="mainmu">
-                            管理者帳號管理 </div>
-                    </a>
-                    <a style="color:#000; font-size:13px; text-decoration:none;" href="?do=menu">
-                        <div class="mainmu">
-                            選單管理 </div>
-                    </a>
+
 
 
                 </div>
                 <div class="dbor" style="margin:3px; width:95%; height:20%; line-height:100px;">
                     <span class="t">進站總人數 :
-                    <?=$Total->find(1)['total'];?></span>
+                        <?=$Total->find(1)['total'];?></span>
                 </div>
             </div>
             <?php
-				$do=$_GET['do']??'title';
+				$do=$_GET['do']??'image';
 				$file="./backend/{$do}.php";
 
 				if(file_exists($file)){
 					include $file;
 				}else{
-					include "./backend/title.php";
+					include "./backend/image.php";
 				}
 				?>
 
